99eef30cb6d7ab976e51b038bddf692d53fd7159
Yale Araujo <yale850@gmail.com>
2023-09-12 13:47:53 -0300

    feat: testing db with express

--- node/index.js
+++ node/index.js
@@ -4,8 +4,22 @@ const app = express()
 
 const port = 3000
 
+const config = {
+    host: 'db',
+    user: 'localhost',
+    password: 'password',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Yale')`
+connection.query(sql)
+connection.end()
+
 app.get('/', (request, response) => {
-    response.send('<h1>Obrigado Deus!<h1>')
+    response.send('<h1>Testando aplicação!<h1>')
 })
 
 app.listen(port, () => {
